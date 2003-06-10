c33febc275e9278a6eccac63301234acd351c6f5
Karl DeBisschop <kdebisschop@users.sourceforge.net>
2003-06-10 04:55:31 +0000

    add comment relating to https://sourceforge.net/tracker/index.php?func=detail&aid=661916&group_id=29880&atid=397597
    git-svn-id: https://nagiosplug.svn.sourceforge.net/svnroot/nagiosplug/nagiosplug/branches/release-1.3.0@542 f882894a-f735-0410-b71e-b25c423dba1c

--- contrib/urlize.pl
+++ contrib/urlize.pl
@@ -5,6 +5,19 @@
 #
 # A wrapper around Nagios plugins that provides a URL link in the output
 #
+#Pay close attention to quoting to ensure that the shell passes the expected
+# data to the plugin. For example, in:
+#
+#    urlize http://example.com/ check_http -H example.com -r 'two words'
+#
+# the shell will remove the single quotes and urlize will see:
+#
+#    urlize http://example.com/ check_http -H example.com -r two words
+#
+# You probably want:
+#
+#    urlize http://example.com/ \"check_http -H example.com -r 'two words'\"
+#
 
 ($#ARGV < 1) && die "Incorrect arguments";
 my $url = shift;
