6f53b99720ebe05d801f814fda823ef7c885f869
Karl DeBisschop <kdebisschop@users.sourceforge.net>
2003-08-06 12:13:03 +0000

    markup for translation, move send_buffer assignment to process_args so process_tcp_request can be moved outside the conditional, replace if/esleif with switch, replace #defines with enum
    git-svn-id: https://nagiosplug.svn.sourceforge.net/svnroot/nagiosplug/nagiosplug/trunk@650 f882894a-f735-0410-b71e-b25c423dba1c

--- plugins/check_overcr.c
+++ plugins/check_overcr.c
@@ -1,64 +1,44 @@
 /******************************************************************************
-*
-* CHECK_OVERCR.C
-*
-* Program: Over-CR collector plugin for Nagios
-* License: GPL
-* Copyright (c) 1999 Ethan Galstad (nagios@nagios.org)
-*
-* $Id$
-*
-* Description:
-*
-* Notes:
-* - This plugin requires that Eric Molitors' Over-CR collector daemon
-*        be running on any UNIX boxes you want to monitor.  Over-CR
-*        is available from * http://www.molitor.org/overcr/
-*
-* Modifications:
-*
-* 08-11-999 Jacob Lundqvist <jaclu@grm.se>
-* Load was presented as a one digit percentage - changed to two digit
-*	value * before load of 11.2 was presented as "1.2%" (not very
-*	high). Warning and Critical params were int's, not very good
-*	for load, changed to doubles, so we can trap loadlimits like
-*	1.5.  Also added more informative LOAD error messages.
-* 
-* License Information:
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*
-*****************************************************************************/
 
-#include "config.h"
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+******************************************************************************/
+
+const char *progname = "check_overcr";
+const char *revision = "$Revision$";
+const char *copyright = "2000-2003";
+const char *email = "nagiosplug-devel@lists.sourceforge.net";
+
 #include "common.h"
 #include "netutils.h"
 #include "utils.h"
 
-#define CHECK_NONE	0
-#define CHECK_LOAD1  	1
-#define CHECK_LOAD5	2
-#define CHECK_LOAD15	4
-#define CHECK_DPU	8
-#define CHECK_PROCS	16
-#define CHECK_NETSTAT	32
-#define CHECK_UPTIME	64
+enum checkvar {
+	NONE,
+	LOAD1,
+	LOAD5,
+	LOAD15,
+	DPU,
+	PROCS,
+	NETSTAT,
+	UPTIME
+};
 
-#define PORT	2000
-
-const char *progname = "check_overcr";
+enum {
+	PORT = 2000
+};
 
 char *server_address = NULL;
 int server_port = PORT;
@@ -66,12 +46,13 @@ double warning_value = 0L;
 double critical_value = 0L;
 int check_warning_value = FALSE;
 int check_critical_value = FALSE;
-int vars_to_check = CHECK_NONE;
+enum checkvar vars_to_check = NONE;
 int cmd_timeout = 1;
 
 int netstat_port = 0;
 char *disk_name = NULL;
 char *process_name = NULL;
+	char send_buffer[MAX_INPUT_BUFFER];
 
 int process_arguments (int, char **);
 void print_usage (void);
@@ -81,9 +62,7 @@ int
 main (int argc, char **argv)
 {
 	int result;
-	char send_buffer[MAX_INPUT_BUFFER];
 	char recv_buffer[MAX_INPUT_BUFFER];
-	char output_message[MAX_INPUT_BUFFER];
 	char temp_buffer[MAX_INPUT_BUFFER];
 	char *temp_ptr = NULL;
 	int found_disk = FALSE;
@@ -109,44 +88,42 @@ main (int argc, char **argv)
 	/* set socket timeout */
 	alarm (socket_timeout);
 
-	result = STATE_OK;
+	result = process_tcp_request2 (server_address,
+	                               server_port,
+	                               send_buffer,
+	                               recv_buffer,
+	                               sizeof (recv_buffer));
 
-	if (vars_to_check == CHECK_LOAD1 || vars_to_check == CHECK_LOAD5
-			|| vars_to_check == CHECK_LOAD15) {
+	switch (vars_to_check) {
 
-		strcpy (send_buffer, "LOAD\r\nQUIT\r\n");
-		result =
-			process_tcp_request2 (server_address, server_port, send_buffer,
-														recv_buffer, sizeof (recv_buffer));
+	case LOAD1:
+	case LOAD5:
+	case LOAD15:
+	
 		if (result != STATE_OK)
-			return result;
+			terminate (result, _("Unknown error fetching load data\n"));
 
 		temp_ptr = (char *) strtok (recv_buffer, "\r\n");
-		if (temp_ptr == NULL) {
-			printf ("Invalid response from server - no load information\n");
-			return STATE_CRITICAL;
-		}
+		if (temp_ptr == NULL)
+			terminate (STATE_CRITICAL, _("Invalid response from server - no load information\n"));
 		load_1min = strtod (temp_ptr, NULL);
+
 		temp_ptr = (char *) strtok (NULL, "\r\n");
-		if (temp_ptr == NULL) {
-			printf ("Invalid response from server after load 1\n");
-			return STATE_CRITICAL;
-		}
+		if (temp_ptr == NULL)
+			terminate (STATE_CRITICAL, _("Invalid response from server after load 1\n"));
 		load_5min = strtod (temp_ptr, NULL);
+
 		temp_ptr = (char *) strtok (NULL, "\r\n");
-		if (temp_ptr == NULL) {
-			printf ("Invalid response from server after load 5\n");
-			return STATE_CRITICAL;
-		}
+		if (temp_ptr == NULL)
+			terminate (STATE_CRITICAL, _("Invalid response from server after load 5\n"));
 		load_15min = strtod (temp_ptr, NULL);
 
-
 		switch (vars_to_check) {
-		case CHECK_LOAD1:
+		case LOAD1:
 			strcpy (temp_buffer, "1");
 			load = load_1min;
 			break;
-		case CHECK_LOAD5:
+		case LOAD5:
 			strcpy (temp_buffer, "5");
 			load = load_5min;
 			break;
@@ -160,30 +137,29 @@ main (int argc, char **argv)
 			result = STATE_CRITICAL;
 		else if (check_warning_value == TRUE && (load >= warning_value))
 			result = STATE_WARNING;
-		sprintf (output_message, "Load %s - %s-min load average = %0.2f",
-						 (result == STATE_OK) ? "ok" : "problem", temp_buffer, load);
-	}
 
+		terminate (result,
+		          _("Load %s - %s-min load average = %0.2f"),
+							 state_text(result),
+		          temp_buffer,
+		          load);
 
-	else if (vars_to_check == CHECK_DPU) {
+			break;
 
-		sprintf (send_buffer, "DISKSPACE\r\n");
-		result =
-			process_tcp_request2 (server_address, server_port, send_buffer,
-														recv_buffer, sizeof (recv_buffer));
+	case DPU:
+
 		if (result != STATE_OK)
-			return result;
+			terminate (result, _("Unknown error fetching disk data\n"));
 
-		for (temp_ptr = (char *) strtok (recv_buffer, " "); temp_ptr != NULL;
-				 temp_ptr = (char *) strtok (NULL, " ")) {
+		for (temp_ptr = (char *) strtok (recv_buffer, " ");
+		     temp_ptr != NULL;
+		     temp_ptr = (char *) strtok (NULL, " ")) {
 
 			if (!strcmp (temp_ptr, disk_name)) {
 				found_disk = TRUE;
 				temp_ptr = (char *) strtok (NULL, "%");
-				if (temp_ptr == NULL) {
-					printf ("Invalid response from server\n");
-					return STATE_CRITICAL;
-				}
+				if (temp_ptr == NULL)
+					terminate (STATE_CRITICAL, _("Invalid response from server\n"));
 				percent_used_disk_space = strtoul (temp_ptr, NULL, 10);
 				break;
 			}
@@ -192,68 +168,54 @@ main (int argc, char **argv)
 		}
 
 		/* error if we couldn't find the info for the disk */
-		if (found_disk == FALSE) {
-			sprintf (output_message, "Error: Disk '%s' non-existent or not mounted",
-							 disk_name);
+		if (found_disk == FALSE)
+			terminate (STATE_CRITICAL,
+			           "Error: Disk '%s' non-existent or not mounted",
+			           disk_name);
+
+		if (check_critical_value == TRUE && (percent_used_disk_space >= critical_value))
 			result = STATE_CRITICAL;
-		}
+		else if (check_warning_value == TRUE && (percent_used_disk_space >= warning_value))
+			result = STATE_WARNING;
 
-		/* else check the disk space used */
-		else {
+		terminate (result, "Disk %s - %lu%% used on %s", state_text(result), percent_used_disk_space, disk_name);
 
-			if (check_critical_value == TRUE
-					&& (percent_used_disk_space >= critical_value)) result =
-					STATE_CRITICAL;
-			else if (check_warning_value == TRUE
-							 && (percent_used_disk_space >= warning_value)) result =
-					STATE_WARNING;
+		break;
 
-			sprintf (output_message, "Disk %s - %lu%% used on %s",
-							 (result == STATE_OK) ? "ok" : "problem",
-							 percent_used_disk_space, disk_name);
-		}
-	}
+	case NETSTAT:
 
-	else if (vars_to_check == CHECK_NETSTAT) {
-
-		sprintf (send_buffer, "NETSTAT %d\r\n", netstat_port);
-		result =
-			process_tcp_request2 (server_address, server_port, send_buffer,
-														recv_buffer, sizeof (recv_buffer));
 		if (result != STATE_OK)
-			return result;
+			terminate (result, _("Unknown error fetching network status\n"));
 
 		port_connections = strtod (recv_buffer, NULL);
 
 		if (check_critical_value == TRUE && (port_connections >= critical_value))
 			result = STATE_CRITICAL;
-		else if (check_warning_value == TRUE
-						 && (port_connections >= warning_value)) result = STATE_WARNING;
+		else if (check_warning_value == TRUE && (port_connections >= warning_value))
+			result = STATE_WARNING;
 
-		sprintf (output_message, "Net %s - %d connection%s on port %d",
-						 (result == STATE_OK) ? "ok" : "problem", port_connections,
-						 (port_connections == 1) ? "" : "s", netstat_port);
-	}
+		terminate (result,
+		           _("Net %s - %d connection%s on port %d"),
+		           state_text(result),
+		           port_connections,
+		           (port_connections == 1) ? "" : "s",
+		           netstat_port);
 
-	else if (vars_to_check == CHECK_PROCS) {
+		break;
 
-		sprintf (send_buffer, "PROCESS %s\r\n", process_name);
-		result =
-			process_tcp_request2 (server_address, server_port, send_buffer,
-														recv_buffer, sizeof (recv_buffer));
+	case PROCS:
+
 		if (result != STATE_OK)
-			return result;
+			terminate (result, _("Unknown error fetching process status\n"));
 
 		temp_ptr = (char *) strtok (recv_buffer, "(");
-		if (temp_ptr == NULL) {
-			printf ("Invalid response from server\n");
-			return STATE_CRITICAL;
-		}
+		if (temp_ptr == NULL)
+			terminate (STATE_CRITICAL, _("Invalid response from server\n"));
+
 		temp_ptr = (char *) strtok (NULL, ")");
-		if (temp_ptr == NULL) {
-			printf ("Invalid response from server\n");
-			return STATE_CRITICAL;
-		}
+		if (temp_ptr == NULL)
+			terminate (STATE_CRITICAL, _("Invalid response from server\n"));
+
 		processes = strtod (temp_ptr, NULL);
 
 		if (check_critical_value == TRUE && (processes >= critical_value))
@@ -261,27 +223,26 @@ main (int argc, char **argv)
 		else if (check_warning_value == TRUE && (processes >= warning_value))
 			result = STATE_WARNING;
 
-		sprintf (output_message, "Process %s - %d instance%s of %s running",
-						 (result == STATE_OK) ? "ok" : "problem", processes,
-						 (processes == 1) ? "" : "s", process_name);
-	}
+		terminate (result,
+		           _("Process %s - %d instance%s of %s running"),
+		           state_text(result),
+		           processes,
+		           (processes == 1) ? "" : "s",
+		           process_name);
+		break;
 
-	else if (vars_to_check == CHECK_UPTIME) {
+	case UPTIME:
 
-		sprintf (send_buffer, "UPTIME\r\n");
-		result =
-			process_tcp_request2 (server_address, server_port, send_buffer,
-														recv_buffer, sizeof (recv_buffer));
 		if (result != STATE_OK)
 			return result;
 
 		uptime_raw_hours = strtod (recv_buffer, NULL);
 		uptime_raw_minutes = (unsigned long) (uptime_raw_hours * 60.0);
 
-		if (check_critical_value == TRUE
-				&& (uptime_raw_minutes <= critical_value)) result = STATE_CRITICAL;
-		else if (check_warning_value == TRUE
-						 && (uptime_raw_minutes <= warning_value)) result = STATE_WARNING;
+		if (check_critical_value == TRUE && (uptime_raw_minutes <= critical_value))
+			result = STATE_CRITICAL;
+		else if (check_warning_value == TRUE && (uptime_raw_minutes <= warning_value))
+			result = STATE_WARNING;
 
 		uptime_days = uptime_raw_minutes / 1440;
 		uptime_raw_minutes %= 1440;
@@ -289,20 +250,23 @@ main (int argc, char **argv)
 		uptime_raw_minutes %= 60;
 		uptime_minutes = uptime_raw_minutes;
 
-		sprintf (output_message, "Uptime %s - Up %d days %d hours %d minutes",
-						 (result == STATE_OK) ? "ok" : "problem", uptime_days,
-						 uptime_hours, uptime_minutes);
-	}
+		terminate (result,
+		           _("Uptime %s - Up %d days %d hours %d minutes"),
+		           state_text(result),
+		           uptime_days,
+		           uptime_hours,
+		           uptime_minutes);
+		break;
 
-	else {
-		strcpy (output_message, "Nothing to check!\n");
-		result = STATE_UNKNOWN;
+	default:
+		terminate (STATE_UNKNOWN, _("Nothing to check!\n"));
+		break;
 	}
 
 	/* reset timeout */
 	alarm (0);
 
-	printf ("%s\n", output_message);
+	printf (_("Reached end of program with no data returned\n"));
 
 	return result;
 }
@@ -360,7 +324,7 @@ process_arguments (int argc, char **argv)
 
 		switch (c) {
 		case '?':									/* print short usage statement if args not parsable */
-			printf ("%s: Unknown argument: %s\n\n", progname, optarg);
+			printf (_("%s: Unknown argument: %s\n\n"), progname, optarg);
 			print_usage ();
 			exit (STATE_UNKNOWN);
 		case 'h':									/* help */
@@ -377,28 +341,36 @@ process_arguments (int argc, char **argv)
 				server_port = atoi (optarg);
 			else
 				terminate (STATE_UNKNOWN,
-									 "Server port an integer (seconds)\nType '%s -h' for additional help\n",
+									 _("Server port an integer (seconds)\nType '%s -h' for additional help\n"),
 									 progname);
 			break;
 		case 'v':									/* variable */
-			if (strcmp (optarg, "LOAD1") == 0)
-				vars_to_check = CHECK_LOAD1;
-			else if (strcmp (optarg, "LOAD5") == 0)
-				vars_to_check = CHECK_LOAD5;
-			else if (strcmp (optarg, "LOAD15") == 0)
-				vars_to_check = CHECK_LOAD15;
-			else if (strcmp (optarg, "UPTIME") == 0)
-				vars_to_check = CHECK_UPTIME;
+			if (strcmp (optarg, "LOAD") == 0) {
+				strcpy (send_buffer, "LOAD\r\nQUIT\r\n");
+				if (strcmp (optarg, "LOAD1") == 0)
+					vars_to_check = LOAD1;
+				else if (strcmp (optarg, "LOAD5") == 0)
+					vars_to_check = LOAD5;
+				else if (strcmp (optarg, "LOAD15") == 0)
+					vars_to_check = LOAD15;
+			}
+			else if (strcmp (optarg, "UPTIME") == 0) {
+				vars_to_check = UPTIME;
+				strcpy (send_buffer, "UPTIME\r\n");
+			}
 			else if (strstr (optarg, "PROC") == optarg) {
-				vars_to_check = CHECK_PROCS;
+				vars_to_check = PROCS;
 				process_name = strscpy (process_name, optarg + 4);
+				sprintf (send_buffer, "PROCESS %s\r\n", process_name);
 			}
 			else if (strstr (optarg, "NET") == optarg) {
-				vars_to_check = CHECK_NETSTAT;
+				vars_to_check = NETSTAT;
 				netstat_port = atoi (optarg + 3);
+				sprintf (send_buffer, "NETSTAT %d\r\n", netstat_port);
 			}
 			else if (strstr (optarg, "DPU") == optarg) {
-				vars_to_check = CHECK_DPU;
+				vars_to_check = DPU;
+				strcpy (send_buffer, "DISKSPACE\r\n");
 				disk_name = strscpy (disk_name, optarg + 3);
 			}
 			else
@@ -421,62 +393,67 @@ process_arguments (int argc, char **argv)
 	}
 	return OK;
 }
-
-
-
-
-
+
 void
 print_usage (void)
 {
-	printf
-		("Usage: %s -H host [-p port] [-v variable] [-w warning] [-c critical] [-t timeout]\n",
-		 progname);
+	printf (_("\
+Usage: %s -H host [-p port] [-v variable] [-w warning] [-c critical]\n\
+  [-t timeout]\n"),
+	        progname);
+	printf (_(UT_HLP_VRS), progname, progname);
 }
-
-
-
-
 
 void
 print_help (void)
 {
-	print_revision (progname, "$Revision$");
-	printf
-		("Copyright (c) 2000 Ethan Galstad/Karl DeBisschop\n\n"
-		 "This plugin attempts to contact the Over-CR collector daemon running on the\n"
-		 "remote UNIX server in order to gather the requested system information. This\n"
-		 "plugin requres that Eric Molitors' Over-CR collector daemon be running on the\n"
-		 "remote server. Over-CR can be downloaded from http://www.molitor.org/overcr\n"
-		 "(This plugin was tested with version 0.99.53 of the Over-CR collector)\n\n");
+	char *myport;
+	asprintf (&myport, "%d", PORT);
+
+	print_revision (progname, revision);
+
+	printf (_("Copyright (c) 1999 Ethan Galstad <nagios@nagios.org>\n"));
+	printf (_(COPYRIGHT), copyright, email);
+
+	printf (_("\
+This plugin attempts to contact the Over-CR collector daemon running on the\n\
+remote UNIX server in order to gather the requested system information.\n\n"));
+
 	print_usage ();
-	printf
-		("\nOptions:\n"
-		 "-H, --hostname=HOST\n"
-		 "   Name of the host to check\n"
-		 "-p, --port=INTEGER\n"
-		 "   Optional port number (default: %d)\n"
-		 "-v, --variable=STRING\n"
-		 "   Variable to check.  Valid variables include:\n"
-		 "     LOAD1         = 1 minute average CPU load\n"
-		 "     LOAD5         = 5 minute average CPU load\n"
-		 "     LOAD15        = 15 minute average CPU load\n"
-		 "     DPU<filesys>  = percent used disk space on filesystem <filesys>\n"
-		 "     PROC<process> = number of running processes with name <process>\n"
-		 "     NET<port>     = number of active connections on TCP port <port>\n"
-		 "     UPTIME        = system uptime in seconds\n"
-		 " -w, --warning=INTEGER\n"
-		 "   Threshold which will result in a warning status\n"
-		 " -c, --critical=INTEGER\n"
-		 "   Threshold which will result in a critical status\n"
-		 " -t, --timeout=INTEGER\n"
-		 "   Seconds before connection attempt times out (default: %d)\n"
-		 "-h, --help\n"
-		 "   Print this help screen\n"
-		 "-V, --version\n"
-		 "   Print version information\n\n"
-		 "Notes:\n"
-		 " - For the available options, the critical threshold value should always be\n"
-		 "   higher than the warning threshold value, EXCEPT with the uptime variable\n"
-		 "   (i.e. lower uptimes are worse).\n", PORT, DEFAULT_SOCKET_TIMEOUT);
+
+	printf (_(UT_HELP_VRSN));
+
+	printf (_(UT_HOST_PORT), 'p', myport);
+
+	printf (_("\
+-v, --variable=STRING\n\
+   Variable to check.  Valid variables include:\n\
+     LOAD1         = 1 minute average CPU load\n\
+     LOAD5         = 5 minute average CPU load\n\
+     LOAD15        = 15 minute average CPU load\n\
+     DPU<filesys>  = percent used disk space on filesystem <filesys>\n\
+     PROC<process> = number of running processes with name <process>\n\
+     NET<port>     = number of active connections on TCP port <port>\n\
+     UPTIME        = system uptime in seconds\n"));
+
+	printf (_("\
+ -w, --warning=INTEGER\n\
+   Threshold which will result in a warning status\n\
+ -c, --critical=INTEGER\n\
+   Threshold which will result in a critical status\n"));
+
+	printf (_(UT_TIMEOUT), DEFAULT_SOCKET_TIMEOUT);
+
+	printf (_("\
+Notes:\n\
+ - For the available options, the critical threshold value should always be\n\
+   higher than the warning threshold value, EXCEPT with the uptime variable\n\n"));
+
+	printf (_("\
+ - This plugin requres that Eric Molitors' Over-CR collector daemon be\n\
+   running on the remote server. Over-CR can be downloaded from\n\
+   http://www.molitor.org/overcr (This plugin was tested with version\n\
+   0.99.53 of the Over-CR collector)\n\n"));
+
+	printf (_(UT_SUPPORT));
 }
