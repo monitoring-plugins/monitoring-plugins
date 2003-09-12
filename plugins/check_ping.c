74c00bde9e99accc870e931d72db9d3d9bf60842
Karl DeBisschop <kdebisschop@users.sourceforge.net>
2003-09-12 11:38:32 +0000

    new output format reported by Patrick Allen <p.allen@brandblue.co.uk>
    git-svn-id: https://nagiosplug.svn.sourceforge.net/svnroot/nagiosplug/nagiosplug/trunk@719 f882894a-f735-0410-b71e-b25c423dba1c

--- plugins/check_ping.c
+++ plugins/check_ping.c
@@ -419,7 +419,8 @@ run_ping (const char *cmd, const char *addr)
 		if(sscanf(buf,"%*d packets transmitted, %*d packets received, +%*d errors, %d%% packet loss",&pl)==1 ||
 			 sscanf(buf,"%*d packets transmitted, %*d packets received, %d%% packet loss",&pl)==1	||
 			 sscanf(buf,"%*d packets transmitted, %*d packets received, %d%% loss, time",&pl)==1 ||
-			 sscanf(buf,"%*d packets transmitted, %*d received, %d%% loss, time", &pl)==1)
+			 sscanf(buf,"%*d packets transmitted, %*d received, %d%% loss, time", &pl)==1 ||
+		   sscanf(buf,"%*d packets transmitted, %*d received, +%*d errors, %d%% packet loss", &pl) == 1)
 			continue;
 
 		/* get the round trip average */
