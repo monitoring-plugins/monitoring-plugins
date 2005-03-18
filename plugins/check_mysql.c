8f81329864bdceb6bc2d1068f8fb5af1d2195916
Matthew Kent <mattkent@users.sourceforge.net>
2005-03-18 03:14:03 +0000

    Replication check support for 4.1.x from Gerrit Beine
    git-svn-id: https://nagiosplug.svn.sourceforge.net/svnroot/nagiosplug/nagiosplug/trunk@1154 f882894a-f735-0410-b71e-b25c423dba1c

--- plugins/check_mysql.c
+++ plugins/check_mysql.c
@@ -123,8 +123,16 @@ main (int argc, char **argv)
 				die (STATE_CRITICAL, "%s\n", slaveresult);
 			}
 
+		} else if (mysql_field_count (&mysql) == 33) {
+			/* mysql >= 4.1.1 */
+			snprintf (slaveresult, SLAVERESULTSIZE, "Slave IO: %s Slave SQL: %s", row[10], row[11]);
+			if (strcmp (row[10], "Yes") != 0 || strcmp (row[11], "Yes") != 0) {
+				mysql_free_result (res);
+				mysql_close (&mysql);
+				die (STATE_CRITICAL, "%s\n", slaveresult);
+			}
 		} else {
-			/* mysql 4.x.x */
+			/* mysql 4.0.x or 4.1.0 */
 			snprintf (slaveresult, SLAVERESULTSIZE, "Slave IO: %s Slave SQL: %s", row[9], row[10]);
 			if (strcmp (row[9], "Yes") != 0 || strcmp (row[10], "Yes") != 0) {
 				mysql_free_result (res);
