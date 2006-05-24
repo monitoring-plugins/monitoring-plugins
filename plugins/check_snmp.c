f7bb6d16e6732f04d8b9de081e23be0c5bb02857
Ton Voon <tonvoon@users.sourceforge.net>
2006-05-24 09:05:35 +0000

    Variables need to be declared at top of code for better portability (Gerhard Lausser)
    git-svn-id: https://nagiosplug.svn.sourceforge.net/svnroot/nagiosplug/nagiosplug/trunk@1399 f882894a-f735-0410-b71e-b25c423dba1c

--- plugins/check_snmp.c
+++ plugins/check_snmp.c
@@ -141,6 +141,7 @@ main (int argc, char **argv)
 	char *p2 = NULL;
 	char *show = NULL;
 	char type[8];
+	char *str[MAX_INPUT_BUFFER];
 
 	setlocale (LC_ALL, "");
 	bindtextdomain (PACKAGE, LOCALEDIR);
@@ -338,7 +339,6 @@ main (int argc, char **argv)
 
 		i++;
 
-		char *str[MAX_INPUT_BUFFER];
 		asprintf(str, "=%s%s;;;; ", show, type ? type : "");
 		strcat(perfstr, *str);
 
