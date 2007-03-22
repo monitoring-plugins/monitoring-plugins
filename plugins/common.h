b6a9796625c49c6bd73bd64066ae5276d74b1dfd
Holger Weiss <hweiss@users.sourceforge.net>
2007-03-22 17:54:16 +0000

    Increase MAX_INPUT_BUFFER from 1k to 8k in order to increase I/O performance (Stefan Meier)
    git-svn-id: https://nagiosplug.svn.sourceforge.net/svnroot/nagiosplug/nagiosplug/trunk@1646 f882894a-f735-0410-b71e-b25c423dba1c

--- plugins/common.h
+++ plugins/common.h
@@ -189,7 +189,7 @@ enum {
 
 enum {
 	DEFAULT_SOCKET_TIMEOUT = 10,	 /* timeout after 10 seconds */
-	MAX_INPUT_BUFFER = 1024,	     /* max size of most buffers we use */
+	MAX_INPUT_BUFFER = 8192,	     /* max size of most buffers we use */
 	MAX_HOST_ADDRESS_LENGTH = 256	 /* max size of a host address */
 };
 
