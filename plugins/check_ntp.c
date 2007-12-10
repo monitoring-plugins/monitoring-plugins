c1b08bca7b9c01d01c805dc9e40a447d4526d2d5
Thomas Guyot-Sionnest <dermoth@users.sourceforge.net>
2007-12-10 04:00:40 +0000

    OOPS! Last commit should have included these files.
    git-svn-id: https://nagiosplug.svn.sourceforge.net/svnroot/nagiosplug/nagiosplug/trunk@1858 f882894a-f735-0410-b71e-b25c423dba1c

--- plugins/check_ntp.c
+++ plugins/check_ntp.c
@@ -797,23 +797,23 @@ int main(int argc, char *argv[]){
 
 	switch (result) {
 		case STATE_CRITICAL :
-			asprintf(&result_line, "NTP CRITICAL:");
+			asprintf(&result_line, _("NTP CRITICAL:"));
 			break;
 		case STATE_WARNING :
-			asprintf(&result_line, "NTP WARNING:");
+			asprintf(&result_line, _("NTP WARNING:"));
 			break;
 		case STATE_OK :
-			asprintf(&result_line, "NTP OK:");
+			asprintf(&result_line, _("NTP OK:"));
 			break;
 		default :
-			asprintf(&result_line, "NTP UNKNOWN:");
+			asprintf(&result_line, _("NTP UNKNOWN:"));
 			break;
 	}
 	if(offset_result == STATE_UNKNOWN){
 		asprintf(&result_line, "%s %s", result_line, _("Offset unknown"));
 		asprintf(&perfdata_line, "");
 	} else {
-		asprintf(&result_line, "%s Offset %.10g secs", result_line, offset);
+		asprintf(&result_line, "%s %s %.10g secs", result_line, _("Offset"), offset);
 		asprintf(&perfdata_line, "%s", perfd_offset(offset));
 	}
 	if (do_jitter) {
