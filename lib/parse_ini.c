60b672d49687b71400275bab010fe779a71c9345
Thomas Guyot-Sionnest <dermoth@users.sourceforge.net>
2008-01-30 09:03:29 +0000

    new standardized GPLv3 header (will be applied to all C files)
    git-svn-id: https://nagiosplug.svn.sourceforge.net/svnroot/nagiosplug/nagiosplug/trunk@1912 f882894a-f735-0410-b71e-b25c423dba1c

--- lib/parse_ini.c
+++ lib/parse_ini.c
@@ -1,3 +1,31 @@
+/*****************************************************************************
+*
+* Nagios-plugins parse_ini library
+* 
+* License: GPL
+* Copyright (c) 2007 nagios-plugins team
+* 
+* Last Modified: $Date$
+*
+* This file is part of Nagios-plugins.
+* 
+* Nagios-plugins is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* Nagios-plugins is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with Nagios-plugins.  If not, see <http://www.gnu.org/licenses/>.
+* 
+* $Id$
+*
+****************************************************************************/
+
 #include "common.h"
 #include "parse_ini.h"
 #include "utils_base.h"
