2306ac38909527e2e6984f5262d151eb4a2a107b
Sebastian Schmidt <sschmidt@interhyp.de>
2013-08-18 12:54:06 +0200

    check_procs: clarify debug message

--- plugins/check_procs.c
+++ plugins/check_procs.c
@@ -238,7 +238,7 @@ main (int argc, char **argv)
 				(!usepid && ((ret = stat_exe(procpid, &statbuf) != -1) && statbuf.st_dev == mydev && statbuf.st_ino == myino) ||
 				 (ret == -1 && errno == ENOENT))) {
 				if (verbose >= 3)
-					 printf("not considering - is myself\n");
+					 printf("not considering - is myself or gone\n");
 				continue;
 			}
 
