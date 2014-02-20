4231415878daedefd707a39e54f7b438238908d6
Davide Madrisan <davide.madrisan@gmail.com>
2014-02-20 13:12:26 +0100

    check_tcp: also display the server addr when host_specified is set. This will help the admins when multiple checks are configured
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- plugins/check_tcp.c
+++ plugins/check_tcp.c
@@ -354,8 +354,13 @@ main (int argc, char **argv)
 			printf("Unexpected response from host/socket on ");
 		else
 			printf("%.3f second response time on ", elapsed_time);
-		if(server_address[0] != '/')
-			printf("port %d", server_port);
+		if(server_address[0] != '/') {
+			if (host_specified)
+				printf("host %s and port %d",
+				       server_address, server_port);
+			else
+				printf("port %d", server_port);
+		}
 		else
 			printf("socket %s", server_address);
 	}
