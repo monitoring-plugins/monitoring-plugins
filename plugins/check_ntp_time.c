e3ef3b31a19d57eb448209ccf2af92bdb7a7be46
Casper Langemeijer <casper.langemeijer@procurios.nl>
2014-04-14 13:50:39 +0200

    Fix bug: return socket_timeout_state on timeout

--- plugins/check_ntp_time.c
+++ plugins/check_ntp_time.c
@@ -418,7 +418,7 @@ double offset_request(const char *host, int *status){
 	}
 
 	if (one_read == 0) {
-		die(STATE_CRITICAL, "NTP CRITICAL: No response from NTP server\n");
+		die(socket_timeout_state, _("NTP %s: No response from NTP server\n"), state_text (socket_timeout_state));
 	}
 
 	/* now, pick the best server from the list */
