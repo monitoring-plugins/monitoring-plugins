3a668c0f87f3ad31f28997ed43b374edf78f6227
Jan Wagner <waja@cyconet.org>
2014-08-20 20:32:09 +0200

    Setting PATH at first

--- plugins-scripts/check_oracle.sh
+++ plugins-scripts/check_oracle.sh
@@ -6,10 +6,11 @@
 #  This Monitoring plugin was created to check Oracle status
 #
 
+PATH="@TRUSTED_PATH@"
+export PATH
 PROGNAME=`basename $0`
 PROGPATH=`echo $0 | sed -e 's,[\\/][^\\/][^\\/]*$,,'`
 REVISION="@NP_VERSION@"
-PATH="@TRUSTED_PATH@"
 
 . $PROGPATH/utils.sh
 
