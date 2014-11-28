0c0a05dc5ac5afcfd06315601545d8fd97eb5764
Jan Wagner <waja@cyconet.org>
2014-11-28 16:34:43 +0100

    check_nwstat: Fixing whitespaces and tabs

--- plugins/check_nwstat.c
+++ plugins/check_nwstat.c
@@ -148,7 +148,7 @@ main(int argc, char **argv) {
 	unsigned long nss7_value=0L;
 	unsigned long total_disk_space=0L;
 	unsigned long used_disk_space=0L;
-  unsigned long percent_used_disk_space=0L;
+	unsigned long percent_used_disk_space=0L;
 	unsigned long purgeable_disk_space=0L;
 	unsigned long non_purgeable_disk_space=0L;
 	unsigned long percent_free_space=0;
@@ -474,30 +474,30 @@ main(int argc, char **argv) {
 			used_disk_space=strtoul(recv_buffer,NULL,10);
 			close(sd);
 			my_tcp_connect (server_address, server_port, &sd);
-      /* get total volume in MB */
+			/* get total volume in MB */
 			asprintf (&send_buffer,"VMS%s\r\n",volume_name);
 			result=send_tcp_request(sd,send_buffer,recv_buffer,sizeof(recv_buffer));
 			if (result!=STATE_OK)
 				return result;
 			total_disk_space=strtoul(recv_buffer,NULL,10);
-      /* calculate percent used on volume */
+			/* calculate percent used on volume */
 			percent_used_disk_space=(unsigned long)(((double)used_disk_space/(double)total_disk_space)*100.0);
 
 			if (check_critical_value==TRUE && percent_used_disk_space >= critical_value)
 				result=STATE_CRITICAL;
 			else if (check_warning_value==TRUE && percent_used_disk_space >= warning_value)
 				result=STATE_WARNING;
-			
+
 			asprintf (&output_message,_("%lu MB (%lu%%) used on volume %s - total %lu MB|Used space in percent on %s=%lu;%lu;%lu;0;100"),
 				used_disk_space,
 				percent_used_disk_space,
 				volume_name,
 				total_disk_space,
 				volume_name,
-        percent_used_disk_space,
-        warning_value,
-        critical_value
-        );
+	percent_used_disk_space,
+	warning_value,
+	critical_value
+	);
 		}
 
 		/* check % free space on volume */
