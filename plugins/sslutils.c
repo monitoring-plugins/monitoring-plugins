8e661a3bd6c6892ed4db926eab769c167b0e9091
Jan Wagner <waja@cyconet.org>
2015-10-04 15:11:40 +0200

    sslutils: Remove superfluous parenthesis for sslv3 function too

--- plugins/sslutils.c
+++ plugins/sslutils.c
@@ -67,7 +67,7 @@ int np_net_ssl_init_with_hostname_version_and_cert(int sd, char *host_name, int 
 		break;
 	case 3: /* SSLv3 protocol */
 #if defined(OPENSSL_NO_SSL3)
-		printf(("%s\n", _("CRITICAL - SSL protocol version 3 is not supported by your SSL library.")));
+		printf("%s\n", _("CRITICAL - SSL protocol version 3 is not supported by your SSL library."));
 		return STATE_CRITICAL;
 #else
 		method = SSLv3_client_method();
