ff525fb9572de6b0fbf41f77e9765a361c482cd4
Sven Rueß <github@sritd.de>
2017-04-16 01:13:50 +0200

    Introduced standard check output function in plugins/sslutils.c

--- plugins/sslutils.c
+++ plugins/sslutils.c
@@ -1,29 +1,29 @@
 /*****************************************************************************
-* 
+*
 * Monitoring Plugins SSL utilities
-* 
+*
 * License: GPL
 * Copyright (c) 2005-2010 Monitoring Plugins Development Team
-* 
+*
 * Description:
-* 
+*
 * This file contains common functions for plugins that require SSL.
-* 
+*
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
-* 
+*
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-* 
-* 
+*
+*
 *****************************************************************************/
 
 #define MAX_CN_LENGTH 256
@@ -54,56 +54,49 @@ int np_net_ssl_init_with_hostname_version_and_cert(int sd, char *host_name, int 
 	switch (version) {
 	case MP_SSLv2: /* SSLv2 protocol */
 #if defined(USE_GNUTLS) || defined(OPENSSL_NO_SSL2)
-		printf("%s\n", _("UNKNOWN - SSL protocol version 2 is not supported by your SSL library."));
-		return STATE_UNKNOWN;
+		return print_singleline_return (STATE_UNKNOWN, _("SSL protocol version 2 is not supported by your SSL library."));
 #else
 		method = SSLv2_client_method();
 		break;
 #endif
 	case MP_SSLv3: /* SSLv3 protocol */
 #if defined(OPENSSL_NO_SSL3)
-		printf("%s\n", _("UNKNOWN - SSL protocol version 3 is not supported by your SSL library."));
-		return STATE_UNKNOWN;
+		return print_singleline_return (STATE_UNKNOWN, _("SSL protocol version 3 is not nupported by your SSL library."));
 #else
 		method = SSLv3_client_method();
 		break;
 #endif
 	case MP_TLSv1: /* TLSv1 protocol */
 #if defined(OPENSSL_NO_TLS1)
-		printf("%s\n", _("UNKNOWN - TLS protocol version 1 is not supported by your SSL library."));
-		return STATE_UNKNOWN;
+		return print_singleline_return (STATE_UNKNOWN, _("TLS protocol version 1 is not supported by your SSL library."));
 #else
 		method = TLSv1_client_method();
 		break;
 #endif
 	case MP_TLSv1_1: /* TLSv1.1 protocol */
 #if !defined(SSL_OP_NO_TLSv1_1)
-		printf("%s\n", _("UNKNOWN - TLS protocol version 1.1 is not supported by your SSL library."));
-		return STATE_UNKNOWN;
+		return print_singleline_return (STATE_UNKNOWN, _("TLS protocol version 1.1 is not supported by your SSL library."));
 #else
 		method = TLSv1_1_client_method();
 		break;
 #endif
 	case MP_TLSv1_2: /* TLSv1.2 protocol */
 #if !defined(SSL_OP_NO_TLSv1_2)
-		printf("%s\n", _("UNKNOWN - TLS protocol version 1.2 is not supported by your SSL library."));
-		return STATE_UNKNOWN;
+		return print_singleline_return (STATE_UNKNOWN, _("TLS protocol version 1.2 is not supported by your SSL library."));
 #else
 		method = TLSv1_2_client_method();
 		break;
 #endif
 	case MP_TLSv1_2_OR_NEWER:
 #if !defined(SSL_OP_NO_TLSv1_1)
-		printf("%s\n", _("UNKNOWN - Disabling TLSv1.1 is not supported by your SSL library."));
-		return STATE_UNKNOWN;
+		return print_singleline_return (STATE_UNKNOWN, _("Disabling TLSv1.1 is not supported by your SSL library."));
 #else
 		options |= SSL_OP_NO_TLSv1_1;
 #endif
 		/* FALLTHROUGH */
 	case MP_TLSv1_1_OR_NEWER:
 #if !defined(SSL_OP_NO_TLSv1)
-		printf("%s\n", _("UNKNOWN - Disabling TLSv1 is not supported by your SSL library."));
-		return STATE_UNKNOWN;
+		return print_singleline_return (STATE_UNKNOWN, _("Disabling TLSv1 is not supported by your SSL library."));
 #else
 		options |= SSL_OP_NO_TLSv1;
 #endif
@@ -130,16 +123,14 @@ int np_net_ssl_init_with_hostname_version_and_cert(int sd, char *host_name, int 
 		initialized = 1;
 	}
 	if ((c = SSL_CTX_new(method)) == NULL) {
-		printf("%s\n", _("CRITICAL - Cannot create SSL context."));
-		return STATE_CRITICAL;
+		return print_singleline_return (STATE_CRITICAL, _("Cannot create SSL context."));
 	}
 	if (cert && privkey) {
 		SSL_CTX_use_certificate_file(c, cert, SSL_FILETYPE_PEM);
 		SSL_CTX_use_PrivateKey_file(c, privkey, SSL_FILETYPE_PEM);
 #ifdef USE_OPENSSL
 		if (!SSL_CTX_check_private_key(c)) {
-			printf ("%s\n", _("CRITICAL - Private key does not seem to match certificate!\n"));
-			return STATE_CRITICAL;
+			return print_singleline_return (STATE_CRITICAL, _("Private key does not seem to match certificate!"));
 		}
 #endif
 	}
@@ -157,15 +148,14 @@ int np_net_ssl_init_with_hostname_version_and_cert(int sd, char *host_name, int 
 		if (SSL_connect(s) == 1) {
 			return OK;
 		} else {
-			printf("%s\n", _("CRITICAL - Cannot make SSL connection."));
+			return print_singleline_return (STATE_CRITICAL, _("Cannot make SSL connection."));
 #  ifdef USE_OPENSSL /* XXX look into ERR_error_string */
 			ERR_print_errors_fp(stdout);
 #  endif /* USE_OPENSSL */
 		}
 	} else {
-			printf("%s\n", _("CRITICAL - Cannot initiate SSL handshake."));
+	        return print_singleline_return (STATE_CRITICAL, _("Cannot initiate SSL handshake."));
 	}
-	return STATE_CRITICAL;
 }
 
 void np_net_ssl_cleanup() {
@@ -193,14 +183,14 @@ int np_net_ssl_read(void *buf, int num) {
 
 int np_net_ssl_check_cert(int days_till_exp_warn, int days_till_exp_crit){
 #  ifdef USE_OPENSSL
-	X509 *certificate=NULL;
-	X509_NAME *subj=NULL;
+	X509 *certificate = NULL;
+	X509_NAME *subj = NULL;
 	char timestamp[50] = "";
-	char cn[MAX_CN_LENGTH]= "";
+	char cn[MAX_CN_LENGTH] = "";
 	char *tz;
-	
-	int cnlen =-1;
-	int status=STATE_UNKNOWN;
+
+	int cnlen = -1;
+	int status = STATE_UNKNOWN;
 
 	ASN1_STRING *tm;
 	int offset;
@@ -212,16 +202,14 @@ int np_net_ssl_check_cert(int days_till_exp_warn, int days_till_exp_crit){
 
 	certificate=SSL_get_peer_certificate(s);
 	if (!certificate) {
-		printf("%s\n",_("CRITICAL - Cannot retrieve server certificate."));
-		return STATE_CRITICAL;
+                return print_singleline_return (STATE_CRITICAL, _("Cannot retrieve server certificate."));
 	}
 
 	/* Extract CN from certificate subject */
 	subj=X509_get_subject_name(certificate);
 
 	if (!subj) {
-		printf("%s\n",_("CRITICAL - Cannot retrieve certificate subject."));
-		return STATE_CRITICAL;
+                return print_singleline_return (STATE_CRITICAL, _("Cannot retrieve certificate subject."));
 	}
 	cnlen = X509_NAME_get_text_by_NID(subj, NID_commonName, cn, sizeof(cn));
 	if (cnlen == -1)
@@ -233,8 +221,7 @@ int np_net_ssl_check_cert(int days_till_exp_warn, int days_till_exp_crit){
 	/* Generate tm structure to process timestamp */
 	if (tm->type == V_ASN1_UTCTIME) {
 		if (tm->length < 10) {
-			printf("%s\n", _("CRITICAL - Wrong time format in certificate."));
-			return STATE_CRITICAL;
+                        return print_singleline_return (STATE_CRITICAL, _("Wrong time format in certificate."));
 		} else {
 			stamp.tm_year = (tm->data[0] - '0') * 10 + (tm->data[1] - '0');
 			if (stamp.tm_year < 50)
@@ -243,8 +230,7 @@ int np_net_ssl_check_cert(int days_till_exp_warn, int days_till_exp_crit){
 		}
 	} else {
 		if (tm->length < 12) {
-			printf("%s\n", _("CRITICAL - Wrong time format in certificate."));
-			return STATE_CRITICAL;
+                        return print_singleline_return (STATE_CRITICAL, _("Wrong time format in certificate."));
 		} else {
 			stamp.tm_year =
 				(tm->data[0] - '0') * 1000 + (tm->data[1] - '0') * 100 +
@@ -279,43 +265,43 @@ int np_net_ssl_check_cert(int days_till_exp_warn, int days_till_exp_crit){
 	tzset();
 
 	if (days_left > 0 && days_left <= days_till_exp_warn) {
-		printf (_("%s - Certificate '%s' expires in %d day(s) (%s).\n"), (days_left>days_till_exp_crit)?"WARNING":"CRITICAL", cn, days_left, timestamp);
 		if (days_left > days_till_exp_crit)
 			status = STATE_WARNING;
 		else
 			status = STATE_CRITICAL;
+
+                X509_free(certificate);
+                return print_singleline_return (status, _("Certificate '%s' expires in %d day(s) (%s)."), cn, days_left, timestamp);
 	} else if (days_left == 0 && time_left > 0) {
 		if (time_left >= 3600)
 			time_remaining = (int) time_left / 3600;
 		else
 			time_remaining = (int) time_left / 60;
 
-		printf (_("%s - Certificate '%s' expires in %u %s (%s)\n"),
-			(days_left>days_till_exp_crit) ? "WARNING" : "CRITICAL", cn, time_remaining,
-			time_left >= 3600 ? "hours" : "minutes", timestamp);
-
 		if ( days_left > days_till_exp_crit)
 			status = STATE_WARNING;
 		else
 			status = STATE_CRITICAL;
+
+                X509_free(certificate);
+                return print_singleline_return (status, _("Certificate '%s' expires in %u %s (%s)."), cn, time_remaining, time_left >= 3600 ? "hours" : "minutes", timestamp);
 	} else if (time_left < 0) {
-		printf(_("CRITICAL - Certificate '%s' expired on %s.\n"), cn, timestamp);
-		status=STATE_CRITICAL;
+                X509_free(certificate);
+                return print_singleline_return (STATE_CRITICAL, _("Certificate '%s' expired on %s."), cn, timestamp);
 	} else if (days_left == 0) {
-		printf (_("%s - Certificate '%s' just expired (%s).\n"), (days_left>days_till_exp_crit)?"WARNING":"CRITICAL", cn, timestamp);
 		if (days_left > days_till_exp_crit)
 			status = STATE_WARNING;
 		else
 			status = STATE_CRITICAL;
+
+                X509_free(certificate);
+                return print_singleline_return (status, _("Certificate '%s' just expired (%s)."), cn, timestamp);
 	} else {
-		printf(_("OK - Certificate '%s' will expire on %s.\n"), cn, timestamp);
-		status = STATE_OK;
+                X509_free(certificate);
+                return print_singleline_return (STATE_OK, _("Certificate '%s' will expire on %s."), cn, timestamp);
 	}
-	X509_free(certificate);
-	return status;
 #  else /* ifndef USE_OPENSSL */
-	printf("%s\n", _("WARNING - Plugin does not support checking certificates."));
-	return STATE_WARNING;
+	return print_singleline_return (STATE_WARNING, "%s", _("Plugin does not support checking certificates."));
 #  endif /* USE_OPENSSL */
 }
 
