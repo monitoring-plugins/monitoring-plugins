cf669f5ff51b746569ded30e990b9d53e5234da0
Lorenz <12514511+RincewindsHat@users.noreply.github.com>
2022-01-22 22:23:13 +0100

    Trivial printf fix and a little bit of code style (#1695)
    * Fix several warnings (and some downright bugs probably) with formating in check_disk
    
    Update to master
    
    * Fix merge error, I forgot the last time
    
    * Fix indentation
    
    Co-authored-by: rincewind <rincewind@vulgrim.de>

--- plugins/check_disk.c
+++ plugins/check_disk.c
@@ -230,8 +230,10 @@ main (int argc, char **argv)
   /* Process for every path in list */
   for (path = path_select_list; path; path=path->name_next) {
     if (verbose >= 3 && path->freespace_percent->warning != NULL && path->freespace_percent->critical != NULL)
-      printf("Thresholds(pct) for %s warn: %f crit %f\n",path->name, path->freespace_percent->warning->end,
-                                                         path->freespace_percent->critical->end);
+      printf("Thresholds(pct) for %s warn: %f crit %f\n",
+        path->name,
+        path->freespace_percent->warning->end,
+        path->freespace_percent->critical->end);
 
     if (verbose >= 3 && path->group != NULL)
       printf("Group of %s: %s\n",path->name,path->group);
