bfd6886a95711d897d30040461b3551293856a73
RincewindsHat <12514511+RincewindsHat@users.noreply.github.com>
2023-03-05 15:37:47 +0100

    Rework maxfd/open_max to avoid unused variables

--- plugins/runcmd.c
+++ plugins/runcmd.c
@@ -88,8 +88,7 @@ extern void die (int, const char *, ...)
  * through this api and thus achieve async-safeness throughout the api */
 void np_runcmd_init(void)
 {
-	if(maxfd == 0)
-		maxfd = open_max();
+  long maxfd = open_max();
 	if(!np_pids) np_pids = calloc(maxfd, sizeof(pid_t));
 }
 
@@ -196,6 +195,7 @@ np_runcmd_open(const char *cmdstring, int *pfd, int *pfderr)
 		/* close all descriptors in np_pids[]
 		 * This is executed in a separate address space (pure child),
 		 * so we don't have to worry about async safety */
+    long maxfd = open_max();
 		for (i = 0; i < maxfd; i++)
 			if(np_pids[i] > 0)
 				close (i);
@@ -223,6 +223,7 @@ np_runcmd_close(int fd)
 	pid_t pid;
 
 	/* make sure this fd was opened by popen() */
+  long maxfd = open_max();
 	if(fd < 0 || fd > maxfd || !np_pids || (pid = np_pids[fd]) == 0)
 		return -1;
 
@@ -246,6 +247,7 @@ runcmd_timeout_alarm_handler (int signo)
 	if (signo == SIGALRM)
 		puts(_("CRITICAL - Plugin timed out while executing system call"));
 
+  long maxfd = open_max();
 	if(np_pids) for(i = 0; i < maxfd; i++) {
 		if(np_pids[i] != 0) kill(np_pids[i], SIGKILL);
 	}
