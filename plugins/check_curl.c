f7687d47cb0841d0bc12ef205fcbaef8bf2e3805
RincewindsHat <12514511+RincewindsHat@users.noreply.github.com>
2023-03-12 19:56:43 +0100

    Fixes for -Wimplicit-fallthrough

--- plugins/check_curl.c
+++ plugins/check_curl.c
@@ -1654,6 +1654,7 @@ process_arguments (int argc, char **argv)
       break;
     case 'R': /* regex */
       cflags |= REG_ICASE;
+			// fall through
     case 'r': /* regex */
       strncpy (regexp, optarg, MAX_RE_SIZE - 1);
       regexp[MAX_RE_SIZE - 1] = 0;
