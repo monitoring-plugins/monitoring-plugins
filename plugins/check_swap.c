8a8ee58e8925019b7532e7d14ebe488bb21fb3e6
RincewindsHat <12514511+RincewindsHat@users.noreply.github.com>
2023-03-16 15:26:52 +0100

    check_swap: Remove unnecessary and problematic includes

--- plugins/check_swap.c
+++ plugins/check_swap.c
@@ -34,9 +34,6 @@ const char *email = "devel@monitoring-plugins.org";
 #include "common.h"
 #include "popen.h"
 #include "utils.h"
-#include <string.h>
-#include <math.h>
-#include <libintl.h>
 
 #ifdef HAVE_DECL_SWAPCTL
 # ifdef HAVE_SYS_PARAM_H
