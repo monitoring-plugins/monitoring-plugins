cbeb04fc5a7e3c632254e8d63711e853e34e1ac7
Jan Wagner <waja@cyconet.org>
2023-05-11 20:54:53 +0000

    clean up the build script

--- .github/mock.sh
+++ .github/mock.sh
@@ -3,9 +3,8 @@
 set -x
 
 export DEBIAN_FRONTEND=noninteractive
-BASE_PATH="/src"
 
-#cd ${BASE_PATH}
+BASE_PATH="/src"
 
 ls -la ${BASE_PATH}/.github/os_detect.sh
 . ${BASE_PATH}/.github/os_detect.sh
@@ -16,7 +15,7 @@ SPEC_DIR="${BASE_PATH}/.github/"
 SOURCE_DIR="."
 SPEC_FILE="${SPEC_DIR}monitoring-plugins.spec"
 
-cd ${BASE_PATH} # && ls -la && dirname ${SPEC_FILE} && ls -la ${SPEC_FILE}
+cd ${BASE_PATH}
 
 dnf -y --setopt="tsflags=nodocs" update && \
   if [ ${distro_id} != "fedora" ]; then dnf -y --setopt="tsflags=nodocs" install epel-release; else platform_id="$(echo ${platform_id} | sed s/^f/fc/)"; fi && \
@@ -32,12 +31,7 @@ if command -v git > /dev/null 2>&1; then
   SRC_RPM="monitoring-plugins-*git.$(echo ${SHA:0:7})*.${platform_id}.src.rpm"
 fi
 mkdir -p "${SRCRPM_DIR}" "${RPM_DIR}"
-#ls -la "$(dirname ${SPEC_FILE})" && ls -la ${SPEC_FILE}
 rpmbuild --undefine=_disable_source_fetch --define "_sourcedir ${SOURCE_DIR}" -ba ${SPEC_FILE}
-#ls -la ${SPEC_FILE} ${SOURCE_DIR}
 mock --dnf --clean --spec ${SPEC_FILE} --sources=${SOURCE_DIR} --result=${SRCRPM_DIR} --build || { cat ${SRCRPM_DIR}/{root,build}.log; exit 1; }
-#cat ${SRCRPM_DIR}/{root,build}.log
-#ls -la ${SPEC_FILE} ${SOURCE_DIR} ${SRCRPM_DIR}
 mock --dnf --clean --sources=${SOURCE_DIR} --result=${RPM_DIR} --rebuild ${SRCRPM_DIR}/${SRC_RPM} || { cat ${RPM_DIR}/{root,build}.log; exit 1; }
 ls -la ${SOURCE_DIR} ${SRCRPM_DIR} ${RPM_DIR}
-#cat ${RPM_DIR}/{root,build}.log
