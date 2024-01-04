735b04eff721a28e791714c0da4c8ac5726bfbcf
RincewindsHat <12514511+RincewindsHat@users.noreply.github.com>
2024-01-04 02:37:26 +0100

    Fix tests

--- plugins/tests/test_check_swap.c
+++ plugins/tests/test_check_swap.c
@@ -10,7 +10,6 @@ void print_help(swap_config config) {
 const char *progname = "test_check_swap";
 
 int main() {
-
 	swap_config config = swap_config_init();
 
 	swap_result test_data = getSwapFromProcMeminfo(config, "./var/proc_meminfo");
@@ -19,6 +18,6 @@ int main() {
 
 	ok(test_data.errorcode == 0, "Test whether we manage to retrieve swap data");
 	ok(test_data.metrics.total == 34233905152, "Is the total Swap correct");
-	ok(test_data.metrics.free == 34231021568, "Is the free Swap correct");
+	ok(test_data.metrics.free == 34233905152, "Is the free Swap correct");
 	ok(test_data.metrics.used == 0, "Is the used Swap correct");
 }
