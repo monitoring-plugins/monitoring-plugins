baa2208a1daa3bf12c6bd7f9bc3646fcff36121f
Lorenz Kästle <12514511+RincewindsHat@users.noreply.github.com>
2024-10-31 14:23:05 +0100

    popen.c: update copyright

--- plugins/popen.c
+++ plugins/popen.c
@@ -3,7 +3,7 @@
  * Monitoring Plugins popen
  *
  * License: GPL
- * Copyright (c) 2005-2007 Monitoring Plugins Development Team
+ * Copyright (c) 2005-2024 Monitoring Plugins Development Team
  *
  * Description:
  *
