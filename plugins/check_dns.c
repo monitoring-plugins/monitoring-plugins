078e9b96adce9a9ae10535e496a743288eb8c9f2
RincewindsHat <12514511+RincewindsHat@users.noreply.github.com>
2024-10-31 03:33:51 +0100

    check_dns: update copyright

--- plugins/check_dns.c
+++ plugins/check_dns.c
@@ -3,7 +3,7 @@
  * Monitoring check_dns plugin
  *
  * License: GPL
- * Copyright (c) 2000-2008 Monitoring Plugins Development Team
+ * Copyright (c) 2000-2024 Monitoring Plugins Development Team
  *
  * Description:
  *
@@ -30,7 +30,7 @@
  *****************************************************************************/
 
 const char *progname = "check_dns";
-const char *copyright = "2000-2008";
+const char *copyright = "2000-2024";
 const char *email = "devel@monitoring-plugins.org";
 
 #include "common.h"
