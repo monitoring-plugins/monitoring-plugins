540d609d845869a5c826f8190bdfed47db1e4259
Lorenz Kästle <12514511+RincewindsHat@users.noreply.github.com>
2024-11-08 10:50:22 +0100

    check_apt: clang-format

--- plugins/check_apt.c
+++ plugins/check_apt.c
@@ -1,33 +1,33 @@
 /*****************************************************************************
-*
-* Monitoring check_apt plugin
-*
-* License: GPL
-* Copyright (c) 2006-2024 Monitoring Plugins Development Team
-*
-* Original author: Sean Finney
-*
-* Description:
-*
-* This file contains the check_apt plugin
-*
-* Check for available updates in apt package management systems
-*
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*****************************************************************************/
+ *
+ * Monitoring check_apt plugin
+ *
+ * License: GPL
+ * Copyright (c) 2006-2024 Monitoring Plugins Development Team
+ *
+ * Original author: Sean Finney
+ *
+ * Description:
+ *
+ * This file contains the check_apt plugin
+ *
+ * Check for available updates in apt package management systems
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
 
 const char *progname = "check_apt";
 const char *copyright = "2006-2024";
@@ -39,7 +39,11 @@ const char *email = "devel@monitoring-plugins.org";
 #include "regex.h"
 
 /* some constants */
-typedef enum { UPGRADE, DIST_UPGRADE, NO_UPGRADE } upgrade_type;
+typedef enum {
+	UPGRADE,
+	DIST_UPGRADE,
+	NO_UPGRADE
+} upgrade_type;
 
 /* Character for hidden input file option (for testing). */
 #define INPUT_FILE_OPT CHAR_MAX + 1
