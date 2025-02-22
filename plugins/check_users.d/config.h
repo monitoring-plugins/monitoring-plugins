fb4f46f93da4ac50654fdcc2f26b2f37c73a9230
Lorenz Kästle <12514511+RincewindsHat@users.noreply.github.com>
2025-02-22 22:39:10 +0100

    Migrate check_users to new ouput infrastructure

---
+++ plugins/check_users.d/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "output.h"
+#include "thresholds.h"
+typedef struct check_users_config {
+	mp_thresholds thresholds;
+
+	bool output_format_is_set;
+	mp_output_format output_format;
+} check_users_config;
+
+check_users_config check_users_config_init() {
+	check_users_config tmp = {
+		.thresholds = mp_thresholds_init(),
+
+		.output_format_is_set = false,
+	};
+	return tmp;
+}
