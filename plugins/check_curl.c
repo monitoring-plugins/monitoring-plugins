246d3a73267eccd609b8f6a967a5a57401284c98
Ahmet Oeztuerk <Ahmet.Oeztuerk@consol.de>
2025-12-16 17:50:17 +0100

    codespell, clang-format and hints fixes

--- plugins/check_curl.c
+++ plugins/check_curl.c
@@ -1011,8 +1011,8 @@ check_curl_config_wrapper process_arguments(int argc, char **argv) {
 			result.config.curl_config.user_auth[MAX_INPUT_BUFFER - 1] = 0;
 			break;
 		case 'x': /* proxy info*/
-			strncpy(result.config.curl_config.proxy, optarg, DEFAULT_BUFFER_SIZE -1);
-			result.config.curl_config.user_auth[DEFAULT_BUFFER_SIZE -1] = 0;
+			strncpy(result.config.curl_config.proxy, optarg, DEFAULT_BUFFER_SIZE - 1);
+			result.config.curl_config.user_auth[DEFAULT_BUFFER_SIZE - 1] = 0;
 			break;
 		case 'b': /* proxy-authorization info */
 			strncpy(result.config.curl_config.proxy_auth, optarg, MAX_INPUT_BUFFER - 1);
@@ -1487,8 +1487,8 @@ void print_help(void) {
 	printf(" %s\n", "-I, --IP-address=ADDRESS");
 	printf("    %s\n",
 		   "IP address or name (use numeric address if possible to bypass DNS lookup).");
-	printf("    %s\n",
-		     "This overwrites the network address of the target while leaving everything else (HTTP headers) as they are");
+	printf("    %s\n", "This overwrites the network address of the target while leaving everything "
+					   "else (HTTP headers) as they are");
 	printf(" %s\n", "-p, --port=INTEGER");
 	printf("    %s", _("Port number (default: "));
 	printf("%d)\n", HTTP_PORT);
@@ -1552,7 +1552,8 @@ void print_help(void) {
 	printf("    %s\n", _("String to expect in the content"));
 	printf(" %s\n", "-u, --url=PATH");
 	printf("    %s\n", _("URL to GET or POST (default: /)"));
-	printf("    %s\n", _("This is the part after the address in a URL, so for \"https://example.com/index.html\" it would be '-u /index.html'"));
+	printf("    %s\n", _("This is the part after the address in a URL, so for "
+						 "\"https://example.com/index.html\" it would be '-u /index.html'"));
 	printf(" %s\n", "-P, --post=STRING");
 	printf("    %s\n", _("URL decoded http POST data"));
 	printf(" %s\n",
@@ -1692,13 +1693,16 @@ void print_help(void) {
 
 	printf("\n %s\n", "CHECK WEBSERVER CONTENT VIA PROXY:");
 	printf(" %s\n", _("Proxies are defined checked using the -x or --proxy parameter:"));
-	printf(" %s\n", _("The environment variables are only checked -x/--proxy arguments are not set:"));
-	printf(" %s\n", _("Depending on the SSL enablement, either http_proxy or https_proxy environment variable is used."));
-	printf(" %s\n", _("These variables can also be given in uppercase, but the lowercase ones will take predence if both are defined."));
+	printf(" %s\n",
+		   _("The environment variables are only checked -x/--proxy arguments are not set:"));
+	printf(" %s\n", _("Depending on the SSL enablement, either http_proxy or https_proxy "
+					  "environment variable is used."));
+	printf(" %s\n", _("These variables can also be given in uppercase, but the lowercase ones will "
+					  "take predence if both are defined."));
 	printf(" %s\n",
 		   _("http_proxy=http://192.168.100.35:3128 ./check_curl -H www.monitoring-plugins.org"));
-	printf(" %s\n",
-		   _("HTTPS_PROXY=http://192.168.100.35:3128 ./check_curl -H www.monitoring-plugins.org --ssl"));
+	printf(" %s\n", _("HTTPS_PROXY=http://192.168.100.35:3128 ./check_curl -H "
+					  "www.monitoring-plugins.org --ssl"));
 	printf(" %s\n", _("legacy proxy requests in check_http style still work:"));
 	printf(" %s\n", _("check_curl -I 192.168.100.35 -p 3128 -u http://www.monitoring-plugins.org/ "
 					  "-H www.monitoring-plugins.org"));
@@ -1708,7 +1712,8 @@ void print_help(void) {
 	printf(" %s\n", _("It is recommended to use an environment proxy like:"));
 	printf(" %s\n",
 		   _("https_proxy=http://192.168.100.35:3128 ./check_curl -H www.verisign.com -S"));
-	printf(" %s\n", _("legacy proxy requests in check_http style might still work, but are frowned upon, so DONT:"));
+	printf(" %s\n", _("legacy proxy requests in check_http style might still work, but are frowned "
+					  "upon, so DONT:"));
 	printf(" %s\n", _("check_curl -I 192.168.100.35 -p 3128 -u https://www.verisign.com/ -S -j "
 					  "CONNECT -H www.verisign.com "));
 	printf(" %s\n", _("all these options are needed: -I <proxy> -p <proxy-port> -u <check-url> "
@@ -1730,7 +1735,8 @@ void print_usage(void) {
 	printf("       [-J <client certificate file>] [-K <private key>] [--ca-cert <CA certificate "
 		   "file>] [-D]\n");
 	printf("       [-w <warn time>] [-c <critical time>] [-t <timeout>] [-L] [-E] [-x <proxy>]\n");
-	printf("       [-a auth] [-b proxy_auth] [-f <ok|warning|critical|follow|sticky|stickyport|curl>]\n");
+	printf("       [-a auth] [-b proxy_auth] [-f "
+		   "<ok|warning|critical|follow|sticky|stickyport|curl>]\n");
 	printf("       [-e <expect>] [-d string] [-s string] [-l] [-r <regex> | -R <case-insensitive "
 		   "regex>]\n");
 	printf("       [-P string] [-m <min_pg_size>:<max_pg_size>] [-4|-6] [-N] [-M <age>]\n");
