1d66396768d16b5ee3a336fcc1e99513d667c832
Lorenz Kästle <12514511+RincewindsHat@users.noreply.github.com>
2026-06-04 23:09:41 +0200

    check_mrtg: fix link

--- plugins/check_mrtg.c
+++ plugins/check_mrtg.c
@@ -436,7 +436,7 @@ void print_help(void) {
 	printf("   %s\n", _("you can always hack the code to make this plugin work for you..."));
 	printf(" %s\n",
 		   _("- MRTG stands for the Multi Router Traffic Grapher.  It can be downloaded from"));
-	printf("   %s\n", "http://ee-staff.ethz.ch/~oetiker/webtools/mrtg/mrtg.html");
+	printf("   %s\n", "https://oss.oetiker.ch/mrtg/");
 
 	printf(UT_SUPPORT);
 }
