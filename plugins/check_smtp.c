c032c3ecbd7320adf5572a2779b76fe5795e09fd
Lorenz Kästle <12514511+RincewindsHat@users.noreply.github.com>
2026-06-19 11:26:50 +0200

    check_smtp: set success value for auth check

--- plugins/check_smtp.c
+++ plugins/check_smtp.c
@@ -481,6 +481,10 @@ int main(int argc, char **argv) {
 	if (config.authtype != NULL) {
 		mp_subcheck sc_auth = mp_subcheck_init();
 
+		// set sucess values here, failure will be set below
+		sc_auth = mp_set_subcheck_default_state(sc_auth, STATE_OK);
+		xasprintf(&sc_auth.output, "Login succeded");
+
 		if (strcmp(config.authtype, "LOGIN") == 0) {
 			char *abuf;
 			int ret;
